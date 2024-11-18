66bf1c82cdac67dcacb0011063c3a353bfabca32
Alec <alecpetitsiejak@gmail.com>
2024-11-18 17:27:58 +0100

    Code de base calendrier

---
+++ src/lib/Calendar.svelte
@@ -0,0 +1,16 @@
+<script>
+    import {onMount} from "svelte";
+
+    let calendar;
+
+    onMount(() => {
+        calendar = document.getElementById("calendar");
+
+        for (let day = 1; day <= 31; day++) {
+            console.log(day);
+            calendar.insertAdjacentHTML("beforeend", `<div class="day">${day}</div>`);
+        }
+    });
+</script>
+
+<div id="calendar"></div>
