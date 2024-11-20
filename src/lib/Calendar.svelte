5e3abf4ad18a0e41f11e80a354695cf7aa628ce6
Alec <alecpetitsiejak@gmail.com>
2024-11-20 13:25:57 +0100

    Calendar final structure

--- src/lib/Calendar.svelte
+++ src/lib/Calendar.svelte
@@ -1,18 +1,56 @@
 <script>
     import {onMount} from "svelte";
-    import {isWeekend, getDayName} from "./date-helpers.js";
+    import {isCurrentDay, isWeekend, getDayName, getMonthName} from "./date-helpers.js";
 
+    let calendarNav;
+    let calendarDaysName;
     let calendar;
+
+    // Initialize current date
+    let currentDate = new Date(Date.now());
 
     onMount(() => {
         calendar = document.getElementById("calendar");
 
+        // Get current month name
+        calendarNav = document.getElementById("calendar-nav");
+        const btnPrevMonth = calendarNav.querySelector("#prev-month");
+        const btnNextMonth = calendarNav.querySelector("#next-month");
+        const labMonth = calendarNav.querySelector("#lab-month");
+
+        labMonth.textContent = getMonthName(currentDate, 0);
+        console.log(labMonth.textContent);
+
+        // Choose month
+        btnPrevMonth.addEventListener("click", () => {
+            console.log("test previous");
+            currentDate.setMonth(currentDate.getMonth() - 1);
+            updateCalendar();
+        })
+
+        btnNextMonth.addEventListener("click", () => {
+            console.log("test next");
+            currentDate.setMonth(currentDate.getMonth() + 1);
+            updateCalendar();
+        })
+
+        // Back to the today month
+        const btnTodayMonth = calendarNav.querySelector("#today-month");
+
+        btnTodayMonth.addEventListener("click", () => {
+            console.log("test today");
+            currentDate = new Date(Date.now());
+            updateCalendar();
+        })
+
         // Display all days name
+        calendarDaysName = document.getElementById("days-name");
+
         for (let day = 1; day <= 7; day++) {
 
             const dayName = getDayName(day);
 
-            calendar.insertAdjacentHTML("beforeend",
+            calendarDaysName.insertAdjacentHTML("beforeend",
                 `<div class="
                     dayName
                     text-center py-3 font-semibold border-b-2 border-gray-300">
@@ -20,35 +58,102 @@
             );
         }
 
-        // Add all days of the month
-        for (let day = 1; day <= 31; day++) {
+        // Update the calendar when select next or previous month
+        function updateCalendar() {
+            // Update month name
+            labMonth.textContent = getMonthName(currentDate);
 
-            // Get de name of the current day
-            //const dayName = getDayName(day);
+            // Clear calendar
+            calendar.innerHTML = "";
 
-            // Test if the current day is a weekend
-            const weekend = isWeekend(day);
+            // Get information on the current month
+            const year = currentDate.getFullYear();
+            const month = currentDate.getMonth() + 1;
 
-            // Insert the day into the DOM tree
-            calendar.insertAdjacentHTML(
-                "beforeend",
-                `<div class="
+            let firstDayIndex = new Date(year + "-" + month + "-01").getDay();
+
+            // Adjust index for french week notation (mon - sun : 1 - 7)
+            if (firstDayIndex === 0) {
+                firstDayIndex = 7;
+            }
+
+            const lastDayIndex = new Date(year, month, 0).getDate() + firstDayIndex;
+            console.log("FIRST DAY : " + firstDayIndex + " LAST DAY : " + lastDayIndex);
+
+            // Add all days of the month
+            let dayNumber = 0;
+            let dayDate;
+
+            for (let day = 1; day <= 35; day++) {
+                // Days of the previous month
+                if (day < firstDayIndex) {
+                    dayNumber = new Date(year, month - 1, 0).getDate() - firstDayIndex + 1 + day;
+                    console.log(dayNumber);
+
+                // Days of the next month
+                } else if (day >= lastDayIndex) {
+                    dayNumber = day - lastDayIndex + 1;
+
+                // Days of the current month
+                } else {
+                    dayNumber = day - firstDayIndex + 1;
+                }
+
+                // Update the date of the day
+                dayDate = new Date(year + "-" + month + "-" + dayNumber);
+
+                // Test if the day is the current day
+                const currentDay = isCurrentDay(dayDate);
+
+                // Test if the day is a weekend
+                const weekend = isWeekend(day);
+
+                // Insert the day into the DOM tree
+                calendar.insertAdjacentHTML(
+                    "beforeend",
+                    `<div class="
                     day
                     ${weekend ? "text-blue-400" : ""}
-                    text-center pt-2 pb-16 border-r-2 border-b-2 border-gray-300">
-                    ${day}</div>`
-            );
+                    flex justify-center text-center pt-2 pb-16 border-r-2 border-b-2 border-gray-300 select-none">
+                        <p class="${currentDay ? "px-1 bg-teal-400 rounded-xl" : ""}">${dayNumber}</p>
+                    </div>`
+                );
+            }
+
+            // Option to select one or more days
+            document.querySelectorAll("#calendar .day").forEach
+            (day => {
+                day.addEventListener("click", event => {
+                    event.currentTarget.classList.toggle("bg-gray-200");
+                });
+            });
         }
 
-        // Option to select one or more days
-        document.querySelectorAll("#calendar .day").forEach
-        (day => {
-            day.addEventListener("click", event => {
-                event.currentTarget.classList.toggle("bg-blue-300");
-            });
-        });
-
+        updateCalendar();
     });
 </script>
 
-<div id="calendar" class="grid grid-cols-7 bg-gray-100"></div>
+<div class="bg-gray-100">
+    <div id="calendar-nav" class="grid grid-cols-7 items-center py-4">
+        <div class="col-start-1 text-center">
+            <input type="button" id="today-month"
+                   class="border border-gray-800 rounded-md text-left px-3 py-1" value="Today" />
+        </div>
+
+        <p id="lab-month" class="col-start-4 text-center">Month</p>
+
+        <div class="col-start-7 text-center">
+            <input type="button" id="prev-month" value="<"
+                   class="border border-gray-800 rounded-md px-3 py-1" />
+
+            <input type="button" id="next-month" value=">"
+                   class="border border-gray-800 rounded-md px-3 py-1" />
+        </div>
+
+    </div>
+
+    <div id="days-name" class="grid grid-cols-7"></div>
+
+    <div id="calendar" class="grid grid-cols-7"></div>
+
+</div>
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -111,38 +111,40 @@
     }
 </script>
 
-<header>
-    <!-- Menu -->
-    <nav class="bg-gray-800 p-4 shadow-md">
-        <div class="container mx-auto flex items-center justify-between">
-            <div class="text-teal-400 text-xl font-semibold">
-                <a href="/">SmartDesk</a>
-            </div>
-            <ul class="flex space-x-6 text-white text-sm font-medium">
-                <li>
-                    <a
-                            href="/"
-                            class="hover:text-teal-400 transition duration-200"
-                    >ACCUEIL</a
-                    >
-                </li>
-                <li>
-                    <a
-                            href="/page-widget"
-                            class="hover:text-teal-400 transition duration-200"
-                    >WIDGETS</a
-                    >
-                </li>
-                <li>
-                    <a
-                            href="/page-account"
-                            class="hover:text-teal-400 transition duration-200"
-                    >COMPTE</a
-                    >
-                </li>
-            </ul>
+<!-- Menu -->
+<nav class="bg-gray-800 px-5 py-4 shadow-md">
+    <div class="items-center flex">
+        <div class="text-teal-400 text-xl font-semibold w-1/3 text-left">
+            <a href="/">SmartDesk</a>
+        </div>
+        <ul class="flex text-white text-sm font-medium w-1/3 text-center justify-center gap-7">
+            <li>
+                <a
+                        href="/"
+                        class="hover:text-teal-400 transition duration-200"
+                >HOME</a
+                >
+            </li>
+            <li>
+                <a
+                        href="/page-widget"
+                        class="hover:text-teal-400 transition duration-200"
+                >WIDGETS</a
+                >
+            </li>
+            <li>
+                <a
+                        href="/page-account"
+                        class="hover:text-teal-400 transition duration-200"
+                >ACCOUNT</a
+                >
+            </li>
+        </ul>
+
+        <div class="w-1/3 text-right">
+
             <button
-                    class="text-sm px-4 py-2 rounded bg-teal-500 hover:bg-teal-600 text-white transition duration-200"
+                    class="text-sm px-4 py-2 rounded bg-teal-500 hover:bg-teal-600 text-white transition duration-200 col-start-3"
                     on:click={() => (connected = !connected)}
             >
                 {#if connected}
@@ -151,157 +153,166 @@
                     Se connecter
                 {/if}
             </button>
-        </div>
-    </nav>
 
-    <!-- Favorites -->
-    <div class="bg-gray-700 p-2 shadow-md">
-        <div class="container mx-auto flex items-center space-x-4">
-            {#each favorites as favorite, index}
-                <a
-                        href={favorite.url}
-                        class="text-gray-200 hover:text-teal-200 transition duration-200 px-2 py-1 rounded hover:bg-gray-600"
-                        on:contextmenu={(e) => openContextMenu(e, index)}
-                >
-                    {favorite.name}
-                </a>
-            {/each}
-            <button
-                    on:click={openAddFavoriteModal}
+        </div>
+
+    </div>
+</nav>
+
+<!-- Favorites -->
+<div class="bg-gray-700 px-5 shadow-md">
+    <div class="container mx-auto flex items-center space-x-4">
+        {#each favorites as favorite, index}
+            <a
+                    href={favorite.url}
                     class="text-gray-200 hover:text-teal-200 transition duration-200 px-2 py-1 rounded hover:bg-gray-600"
+                    on:contextmenu={(e) => openContextMenu(e, index)}
             >
-                +
-            </button>
+                {favorite.name}
+            </a>
+        {/each}
+        <button
+                on:click={openAddFavoriteModal}
+                class="text-gray-200 hover:text-teal-200 transition duration-200 px-2 py-1 rounded hover:bg-gray-600"
+        >
+            +
+        </button>
+    </div>
+</div>
+
+<!-- Context Menu -->
+{#if showContextMenu}
+    <div
+            class="absolute bg-gray-800 text-white p-2 rounded shadow-md context-menu"
+            style="top: {contextMenuPosition.y}px; left: {contextMenuPosition.x}px;"
+    >
+        {#if contextIndex !== null}
+            <button
+                    on:click={() => editFavoriteItem(contextIndex)}
+                    class="block px-4 py-2 hover:bg-gray-600">Edit
+            </button
+            >
+            <button
+                    on:click={() => deleteFavorite(contextIndex)}
+                    class="block px-4 py-2 hover:bg-gray-600 text-red-400"
+            >Delete
+            </button
+            >
+        {:else}
+            <button
+                    on:click={addNewFavoriteFromContextMenu}
+                    class="block px-4 py-2 hover:bg-gray-600"
+            >New Favorite
+            </button
+            >
+        {/if}
+    </div>
+{/if}
+
+<!-- Modals New Favorite -->
+{#if showModal}
+    <div
+            class="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center"
+    >
+        <div class="bg-white p-4 rounded shadow-md w-96">
+            <h2 class="text-lg font-semibold mb-4">Add New Favorite</h2>
+            <div class="mb-4">
+                <label
+                        for="favorite-name"
+                        class="block text-sm font-medium text-gray-700"
+                >Name</label
+                >
+                <input
+                        id="favorite-name"
+                        type="text"
+                        bind:value={newFavoriteName}
+                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"
+                />
+                {#if nameError}
+                    <p class="text-red-500 text-sm mt-1">{nameError}</p>
+                {/if}
+            </div>
+            <div class="mb-4">
+                <label
+                        for="favorite-url"
+                        class="block text-sm font-medium text-gray-700"
+                >URL</label
+                >
+                <input
+                        id="favorite-url"
+                        type="text"
+                        bind:value={newFavoriteUrl}
+                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"
+                />
+                {#if urlError}
+                    <p class="text-red-500 text-sm mt-1">{urlError}</p>
+                {/if}
+            </div>
+            <div class="flex justify-end space-x-2">
+                <button
+                        on:click={closeAddFavoriteModal}
+                        class="px-4 py-2 bg-gray-300 hover:bg-gray-400 rounded"
+                >Cancel
+                </button
+                >
+                <button
+                        on:click={addFavorite}
+                        class="px-4 py-2 bg-teal-500 hover:bg-teal-600 text-white rounded"
+                >Save
+                </button
+                >
+            </div>
         </div>
     </div>
+{/if}
 
-    <!-- Context Menu -->
-    {#if showContextMenu}
-        <div
-                class="absolute bg-gray-800 text-white p-2 rounded shadow-md context-menu"
-                style="top: {contextMenuPosition.y}px; left: {contextMenuPosition.x}px;"
-        >
-            {#if contextIndex !== null}
+<!-- Modals Edit Favorite -->
+{#if showEditModal}
+    <div
+            class="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center"
+    >
+        <div class="bg-white p-4 rounded shadow-md w-96">
+            <h2 class="text-lg font-semibold mb-4">Edit Favorite</h2>
+            <div class="mb-4">
+                <label
+                        for="edit-favorite-name"
+                        class="block text-sm font-medium text-gray-700"
+                >Name</label
+                >
+                <input
+                        id="edit-favorite-name"
+                        type="text"
+                        bind:value={editFavoriteName}
+                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"
+                />
+            </div>
+            <div class="mb-4">
+                <label
+                        for="edit-favorite-url"
+                        class="block text-sm font-medium text-gray-700"
+                >URL</label
+                >
+                <input
+                        id="edit-favorite-url"
+                        type="text"
+                        bind:value={editFavoriteUrl}
+                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"
+                />
+            </div>
+            <div class="flex justify-end space-x-2">
                 <button
-                        on:click={() => editFavoriteItem(contextIndex)}
-                        class="block px-4 py-2 hover:bg-gray-600">Edit</button
+                        on:click={closeEditFavoriteModal}
+                        class="px-4 py-2 bg-gray-300 hover:bg-gray-400 rounded"
+                >Cancel
+                </button
                 >
                 <button
-                        on:click={() => deleteFavorite(contextIndex)}
-                        class="block px-4 py-2 hover:bg-gray-600 text-red-400"
-                >Delete</button
+                        on:click={saveEdit}
+                        class="px-4 py-2 bg-teal-500 hover:bg-teal-600 text-white rounded"
+                >Save
+                </button
                 >
-            {:else}
-                <button
-                        on:click={addNewFavoriteFromContextMenu}
-                        class="block px-4 py-2 hover:bg-gray-600"
-                >New Favorite</button
-                >
-            {/if}
-        </div>
-    {/if}
-
-    <!-- Modals New Favorite -->
-    {#if showModal}
-        <div
-                class="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center"
-        >
-            <div class="bg-white p-4 rounded shadow-md w-96">
-                <h2 class="text-lg font-semibold mb-4">Add New Favorite</h2>
-                <div class="mb-4">
-                    <label
-                            for="favorite-name"
-                            class="block text-sm font-medium text-gray-700"
-                    >Name</label
-                    >
-                    <input
-                            id="favorite-name"
-                            type="text"
-                            bind:value={newFavoriteName}
-                            class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"
-                    />
-                    {#if nameError}
-                        <p class="text-red-500 text-sm mt-1">{nameError}</p>
-                    {/if}
-                </div>
-                <div class="mb-4">
-                    <label
-                            for="favorite-url"
-                            class="block text-sm font-medium text-gray-700"
-                    >URL</label
-                    >
-                    <input
-                            id="favorite-url"
-                            type="text"
-                            bind:value={newFavoriteUrl}
-                            class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"
-                    />
-                    {#if urlError}
-                        <p class="text-red-500 text-sm mt-1">{urlError}</p>
-                    {/if}
-                </div>
-                <div class="flex justify-end space-x-2">
-                    <button
-                            on:click={closeAddFavoriteModal}
-                            class="px-4 py-2 bg-gray-300 hover:bg-gray-400 rounded"
-                    >Cancel</button
-                    >
-                    <button
-                            on:click={addFavorite}
-                            class="px-4 py-2 bg-teal-500 hover:bg-teal-600 text-white rounded"
-                    >Save</button
-                    >
-                </div>
             </div>
         </div>
-    {/if}
-
-    <!-- Modals Edit Favorite -->
-    {#if showEditModal}
-        <div
-                class="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center"
-        >
-            <div class="bg-white p-4 rounded shadow-md w-96">
-                <h2 class="text-lg font-semibold mb-4">Edit Favorite</h2>
-                <div class="mb-4">
-                    <label
-                            for="edit-favorite-name"
-                            class="block text-sm font-medium text-gray-700"
-                    >Name</label
-                    >
-                    <input
-                            id="edit-favorite-name"
-                            type="text"
-                            bind:value={editFavoriteName}
-                            class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"
-                    />
-                </div>
-                <div class="mb-4">
-                    <label
-                            for="edit-favorite-url"
-                            class="block text-sm font-medium text-gray-700"
-                    >URL</label
-                    >
-                    <input
-                            id="edit-favorite-url"
-                            type="text"
-                            bind:value={editFavoriteUrl}
-                            class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-teal-500 focus:border-teal-500 sm:text-sm"
-                    />
-                </div>
-                <div class="flex justify-end space-x-2">
-                    <button
-                            on:click={closeEditFavoriteModal}
-                            class="px-4 py-2 bg-gray-300 hover:bg-gray-400 rounded"
-                    >Cancel</button
-                    >
-                    <button
-                            on:click={saveEdit}
-                            class="px-4 py-2 bg-teal-500 hover:bg-teal-600 text-white rounded"
-                    >Save</button
-                    >
-                </div>
-            </div>
-        </div>
-    {/if}
-</header>
+    </div>
+{/if}
