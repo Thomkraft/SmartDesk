3b02eaa216bf927026e1c0acf54e4330b0d6c93f
Thomkraft <69580958+Thomkraft@users.noreply.github.com>
2025-02-20 16:54:07 +0100

    Update Header.svelte

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -205,12 +205,9 @@
 
             {#if $isConnected}
                 <button
-                    class="text-sm px-3 py-1 rounded bg-teal-400 hover:bg-teal-500 text-white transition duration-200"
-                    on:click={disconectionVerif}
                         class="text-sm py-1 rounded transition duration-200"
                         aria-label="Disconnect"
                         on:click={disconectionVerif}
-
                 >
                     <svg class="fill-gray-50 hover:fill-teal-400" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px">
                         <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z"/>
